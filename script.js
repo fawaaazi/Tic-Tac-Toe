967e68469b49711f8d9d576f24bd716c8fd68af0
fawaaazi <muhammedfawas90@gmail.com>
2025-06-24 22:18:16 +0530

    created the function for the creation of the players and also started to buid the gameControl obj

--- script.js
+++ script.js
@@ -8,3 +8,37 @@ const GameBoard = (function (){
     }
     return{ getGameBoard, placeMarker }
 })();
+
+function createPlayer(name , marker){
+    return{name, marker}
+}
+
+const GameControl = (function(){
+    const board = GameBoard.getGameBoard();
+
+    const player1 = createPlayer("fawas", "X");
+    const player2 = createPlayer("someone", "O");
+
+    let gameOver = false;
+
+    const checkWin = () => {
+        const isWin = (
+            /*horizontal wins*/
+            board[0][0] === board[0][1] &&  board[0][0] === board[0][2] &&  board[0][1] === board[0][2] ||
+            board[1][0] === board[1][1] &&  board[1][0] === board[1][2] &&  board[1][1] === board[1][2] ||
+            board[2][0] === board[2][1] &&  board[2][0] === board[2][2] &&  board[2][1] === board[2][2] ||
+
+            /*vertical wins*/
+            board[0][0] === board[1][0] &&  board[0][0] === board[2][0] &&  board[1][0] === board[2][0] ||
+            board[0][1] === board[1][1] &&  board[0][1] === board[2][1] &&  board[1][1] === board[2][1] ||
+            board[0][2] === board[1][2] &&  board[0][2] === board[2][2] &&  board[1][2] === board[2][2] ||
+
+            /*diagonal wins*/
+            board[0][0] === board[1][1] &&  board[0][0] === board[2][2] &&  board[1][1] === board[2][2] ||
+            board[0][2] === board[1][1] &&  board[0][2] === board[2][0] &&  board[1][1] === board[2][0]
+        );
+        return isWin;
+
+    }
+
+})();
